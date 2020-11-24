9133266d72d048d18f062fc237146e8f282cc09a
Damir <dkolobaric@gmail.com>
2020-11-24 17:18:22 +0100

    navigation with event binding

--- first-project/src/app/app.component.ts
+++ first-project/src/app/app.component.ts
@@ -7,4 +7,9 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
 
+  loadedFeature = 'recipes';
+
+  onNavigated(feature: string) {
+    this.loadedFeature = feature;
+  }
 }
