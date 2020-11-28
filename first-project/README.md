a88523a6dd5cb2195df367c73f41f0083bca1201
Damir <dkolobaric@gmail.com>
2020-11-28 11:58:36 +0100

    dropdown

--- first-project/README.md
+++ first-project/README.md
@@ -25,3 +25,9 @@ Run `ng e2e` to execute the end-to-end tests via [Protractor](http://www.protrac
 ## Further help
 
 To get more help on the Angular CLI use `ng help` or go check out the [Angular CLI Overview and Command Reference](https://angular.io/cli) page.
+
+## If error
+
+An unhandled exception occurred: Cannot find module '@angular-devkit/build-angular/package.json
+
+Run npm install --save-dev @angular-devkit/build-angular
--- first-project/src/app/app.module.ts
+++ first-project/src/app/app.module.ts
@@ -11,6 +11,7 @@ import { RecipeItemComponent } from './recipes/recipes-list/recipe-item/recipe-i
 import { ShoppingListComponent } from './shopping-list/shopping-list.component';
 import { ShoppingEditComponent } from './shopping-list/shopping-edit/shopping-edit.component';
 import { FormsModule } from '@angular/forms';
+import { DropdownDirective } from './shared/dropdown.directive';
 
 
 @NgModule({
@@ -22,7 +23,8 @@ import { FormsModule } from '@angular/forms';
     RecipeDetailComponent,
     RecipeItemComponent,
     ShoppingListComponent,
-    ShoppingEditComponent
+    ShoppingEditComponent,
+    DropdownDirective
   ],
   imports: [
     BrowserModule,
