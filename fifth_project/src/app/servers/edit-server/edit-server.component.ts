39bb71a544d13e68daf90c289a24c9923c3e8067
Damir <dkolobaric@gmail.com>
2020-12-09 17:29:49 +0100

    redirecting routes

--- fifth_project/src/app/servers/edit-server/edit-server.component.ts
+++ fifth_project/src/app/servers/edit-server/edit-server.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Params } from '@angular/router';
 
 import { ServersService } from '../servers.service';
 
@@ -12,10 +12,18 @@ export class EditServerComponent implements OnInit {
   server: { id: number, name: string, status: string };
   serverName = '';
   serverStatus = '';
+  allowEdit = false;
 
   constructor(private serversService: ServersService, private route: ActivatedRoute) { }
 
   ngOnInit() {
+    this.route.queryParams.subscribe(
+      (queryParams: Params) => {
+        console.log(queryParams['allowEdit']);
+        this.allowEdit = queryParams['allowEdit'] === '1' ? true : false;
+      }
+    );
+    this.route.fragment.subscribe();
     this.server = this.serversService.getServer(1);
     this.serverName = this.server.name;
     this.serverStatus = this.server.status;
