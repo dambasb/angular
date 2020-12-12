1b01456fa8450951f1107eb11612051d49c6d2c9
Damir <dkolobaric@gmail.com>
2020-12-12 11:57:50 +0100

    guards

--- fifth_project/src/app/app-routing.module.ts
+++ fifth_project/src/app/app-routing.module.ts
@@ -1,8 +1,10 @@
 import { NgModule } from "@angular/core";
 import { RouterModule, Routes } from "@angular/router";
+import { AuthGuard } from "./auth-guard.service";
 
 import { HomeComponent } from "./home/home.component";
 import { PageNotFoundComponent } from "./page-not-found/page-not-found.component";
+import { CanDeactivateGuard } from "./servers/edit-server/can-deactivate-guard.service";
 import { EditServerComponent } from "./servers/edit-server/edit-server.component";
 import { ServerComponent } from "./servers/server/server.component";
 import { ServersComponent } from "./servers/servers.component";
@@ -17,9 +19,13 @@ const appRouts: Routes = [
     ]
   },
   {
-    path: 'servers', component: ServersComponent, children: [
+    path: 'servers',
+    canActivateChild: [AuthGuard],
+    //canActivate: [AuthGuard],
+    component: ServersComponent,
+    children: [
       { path: ':id', component: ServerComponent },
-      { path: ':id/edit', component: EditServerComponent }
+      { path: ':id/edit', component: EditServerComponent, canDeactivate: [CanDeactivateGuard] }
     ]
   },
   { path: 'not-found', component: PageNotFoundComponent },
